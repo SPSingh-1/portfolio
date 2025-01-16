665be35db243a45abc55b2d7cca63d015f006892
Sp-Rajjputt <sp.rajjputt@gmail.com>
2025-01-16 10:38:42 +0530

    Navbar complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Service from "./components/Service";
+import Project from "./components/Project";
+import ContactUs from "./components/ContactUs";
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  // Link,
+} from "react-router-dom";
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+  }
+
+  const toggleMode = (cls)=>{
+    removeBodyClasses()
+    console.log(cls)
+    document.body.classList.add('bg-'+cls)
+      if(mode === 'light'){
+        setMode ('dark');
+        document.body.style.backgroundColor ='#042743';
+    }
+      else{
+        setMode ('light');
+        document.body.style.backgroundColor ='white';
+      }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <Router>
+      <Navbar title="Portfolio" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+      <Routes>
+          <Route exact path="/"  element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/service" element={<Service/>}/>
+          <Route exact path="/project" element={<Project/>}/>
+          <Route exact path="/contactus" element={<ContactUs/>}/>
+      </Routes>
+      </div>
+      
+    </Router>
+    </>
   );
 }
 
